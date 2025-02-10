3a770ceea4406fe650bc79fc5c20daf6fbb041d0
AliKhachab <a.khachab620@gmail.com>
2025-02-10 18:58:47 -0500

    finalized code

--- script.py
+++ script.py
@@ -6,16 +6,16 @@ Assignment 1 - A* Search on 8 Puzzle
 
 Please note that this was done on Python version 3.10.12. If it does not work for you, you might be on an older version.
 """
-from collections import deque
-
+import heapq
+from copy import deepcopy
 
 class State:
-    def __init__(self, boardState, parentState, move, depth, cost):
+    def __init__(self, boardState, parentState, move, depth: int, cost: int):
         self.boardState = boardState
         self.parentState = parentState
         self.move = move
-        self.depth = depth
-        self.cost = cost # depth + heuristic = cost
+        self.depth = depth # "g" in the A* algorithm
+        self.cost = cost # depth + heuristic = cost (basically, f (cost) = g (depth) + h (calculated from heuristic() function)
     
     def __lt__(self, other): # For comparison purposes (i.e. if state1 < state2)
         return self.cost < other.cost
@@ -25,14 +25,15 @@ class State:
             print(self.boardState[i])
 
     def isGoalState(self) -> bool:
-        if self.boardState == EightPuzzle.FINAL_BOARD_STATE:
+        if str(self.boardState) == str(EightPuzzle.FINAL_BOARD_STATE):
             return True
         return False
 
 class EightPuzzle:
+    # Common variables
     FINAL_BOARD_STATE = [ 
         ["1", "2", "3"], 
-        ["8", "X"v, "4"], 
+        ["8", "X", "4"], 
         ["7", "6", "5"] 
     ]
     FINAL_BOARD_STATE_DICT = { # Keep track of all the positions of the final numbers that we want
@@ -46,12 +47,8 @@ class EightPuzzle:
         "6": (2, 1),
         "5": (2, 2)
     }
-    MOVES = {
-        "UP": (-1, 0),
-        "DOWN": (1, 0),
-        "LEFT": (0, -1),
-        "RIGHT": (0, 1)
-    }
+    MOVES = { "UP": (-1, 0), "DOWN": (1, 0), "LEFT": (0, -1), "RIGHT": (0, 1) }
+    
     @staticmethod
     def generateBoard():
         """
@@ -65,27 +62,23 @@ class EightPuzzle:
         while True:
             try:
                 for i in range(3):
-                    for j in range(3):
+                    for _ in range(3):
                         number = input(f"Please enter a number between 1-8 for the 8-puzzle board. If you would like to make it an empty space, please write X.\nThe current values in the board are: {str(setOfNumsInBoard)}. Do not repeat numbers.\n")
                         if (number in setOfNumsInBoard) or (number != "X" and (int(number) < 1 or int(number) > 8)) or (not number.isnumeric() and number != "X"):
                             raise ValueError
                         else: 
                             board[i].append(number)
                             setOfNumsInBoard.add(number)
-                
-                print(f"Your board looks like this:\n{str(board[0])}\n{str(board[1])}\n{str(board[2])}")
                 break
             except ValueError:
                 print("Please enter a valid input. Ensure that you did not repeat yourself, that the numbers are all between 1-8, and that the empty space is noted as 'X'.")
         return board
-    
-    @staticmethod
-    def checkValidMove():
-        pass
-
 
     @staticmethod
     def heuristicOfState(state: State) -> int:
+        """
+        This function generates the Manhattan distance of the current state.
+        """
         distance = 0
         for row in range(3):
             for col in range(3):
@@ -95,6 +88,35 @@ class EightPuzzle:
                 else:
                     continue
         return distance
+    
+    @staticmethod
+    def generatePossibleMoves(state: State) -> list[State]:
+        """
+        Find a list of all possible moves that can be made in the current state.
+        First, find the position of the empty space (X). Then check all possible adjacent tiles (up down left right) to see if it does not go out of bounds of the arrays.
+        If it is, add it to the list of possible moves.
+        """
+        res = []
+        emptySpaceXValue = None
+        emptySpaceYValue = None
+        for i in range(3):
+            for j in range(3):
+                if state.boardState[i][j] == "X":
+                    emptySpaceXValue = i
+                    emptySpaceYValue = j
+                    break
+                
+        for move in EightPuzzle.MOVES:
+            newRow = emptySpaceXValue + EightPuzzle.MOVES[move][0]
+            newCol = emptySpaceYValue + EightPuzzle.MOVES[move][1]
+            if (newRow >= 0 and newRow < 3) and (newCol >= 0 and newCol < 3):
+                copyBoard = deepcopy(state.boardState) # I had an issue with this part where I accidentally made a shallow copy instead of a deep copy of the array. This caused the board to be modified in the parent state as well, meaning that the X would only move along the column it started in..
+                copyBoard[emptySpaceXValue][emptySpaceYValue], copyBoard[newRow][newCol] = copyBoard[newRow][newCol], copyBoard[emptySpaceXValue][emptySpaceYValue]
+                # swap the positions of X and the space adjacent to it (up down left right)
+                newDepth = state.depth + 1
+                newCost = newDepth + EightPuzzle.heuristicOfState(State(boardState=copyBoard, parentState=state, move=move, depth=newDepth, cost=0))
+                res.append(State(boardState = copyBoard, parentState = state, move = move, depth = newDepth, cost = newCost))                
+        return res
 
     @staticmethod
     def eightPuzzleSolver() -> None:
@@ -111,9 +133,32 @@ class EightPuzzle:
             print("The board is already in the final state. No moves needed.")
             return
         
-        # Test heuristic
-        print(EightPuzzle.heuristicOfState(State(board, None, None, 0, 0)))
+        frontier = []
+        initialState = State(boardState = board, parentState = None, move = None, depth = 0, cost = 0)
+        heapq.heappush(frontier, initialState)
+        seen = set()
+        seen.add(str(frontier[0].boardState)) # You will notice later I use a string repr. of the list to check if it is in the set. This is because lists are not hashable, so I couldn't add it to the set in the first place.
+
+        while frontier:
+            currState = heapq.heappop(frontier)
+            print(f"Current depth: {currState.depth}\nCurrent cost: {currState.cost}\nCurrent board state:")
+            currState.printBoard()
+            if currState.isGoalState():
+                print("Goal state reached")
+                return True
+            else:
+                possibleNextMoves = EightPuzzle.generatePossibleMoves(currState)
+                print(f"Seen states: {seen}")
+                # print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+                for newState in possibleNextMoves:
+                    if str(newState.boardState) not in seen:
+                        heapq.heappush(frontier, newState) # this is what the __lt__ function is for
+                        seen.add(str(newState.boardState))
+                        # print("--\nAdding to frontier:")
+                        # newState.printBoard()
+                # print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+        return False
 
 
 if __name__ == "__main__":
-    EightPuzzle.eightPuzzleSolver()
+    print("No solution") if not EightPuzzle.eightPuzzleSolver() else print("Solution displayed above")
